b54fa015699a931a93e0fadd0dec39ab8eea8d82
junyorff12 <junyorff12@gmail.com>
2023-08-08 18:18:24 -0300

    O saldo deve ser maior ou igual ao valor da compra :)

--- src/CartaoDeCredito.java
+++ src/CartaoDeCredito.java
@@ -13,7 +13,7 @@ public class CartaoDeCredito {
     }
 
     public boolean lancaCompra(Compra compra) {
-        if(this.saldo > compra.getValor()){
+        if(this.saldo >= compra.getValor()){
             this.saldo -= compra.getValor();
             this.compras.add(compra);
             return true;
